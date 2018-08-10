8b94d8c5f3cb45228a6bd2683c140f283aa5345a
Martin Møller Skarbiniks Pedersen <martin@linux.com>
2018-08-11 01:04:26 +0200

    initial commit

---
+++ A005132/A005132.R
@@ -0,0 +1,36 @@
+library(hashmap)
+
+in_a_interval <- function(v) {
+  FALSE
+}
+
+add_value <- function(v) {
+}
+
+low <- hashmap(0, 0)
+high <- hashmap(0, 0)
+
+step <- 1
+value <- 0
+
+cat("0 0\n")
+while (step < 10) {
+	if ((value-step>0) && (!in_a_interval(value-step))) {
+		value <- value - step
+	} else {
+		value <- value + step
+	}
+	cat(step,value,"\n")
+	add_value(value)
+	step <- step + 1
+}
+
+
+
+
+
+
+
+
+
+
