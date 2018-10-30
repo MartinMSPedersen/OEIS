aa0a2a745ccd1b9b29af706c9372e292ac82073a
Martin M. S. Pedersen <traxplayer@gmail.com>
2018-10-30 12:25:18 +0100

    Create a 64x64 png picture

--- A000002/A000002.R
+++ A000002/A000002.R
@@ -1,8 +1,11 @@
 # computes and store the first 1.000.000 values of the Kolakoski sequence
+library(magick)
 
-SIZE <- 1e6
-kolakoski <- vector("integer", SIZE)
-OUTPUT <- TRUE
+SIZE <- 64
+SIZE_SQUARE <- SIZE*SIZE
+
+kolakoski <- vector("integer", SIZE_SQUARE)
+OUTPUT <- FALSE
 
 
 # init
@@ -12,7 +15,7 @@ kolakoski[3] <- 2
 read_idx <- 3
 write_idx <- 4
 
-while (write_idx < SIZE) {
+while (write_idx < SIZE_SQUARE) {
 	kolakoski[write_idx] <- 3-kolakoski[write_idx-1]
 	write_idx <- write_idx + 1
 	if (kolakoski[read_idx] == 2) {
@@ -31,5 +34,11 @@ if (OUTPUT) {
 	}
 }
 
-
-	
+kolakoski_cum <- cumsum(kolakoski*2-3)
+k <- matrix(as.logical(kolakoski-1),SIZE, byrow = TRUE)
+k <- t(apply(k,2,rev))
+png("kolakoski-64x64.png", width = SIZE*8, height = SIZE*8)
+#  no margin
+par(mar = rep(0, 4))
+image(k, axes = FALSE, col = grey(seq(0, 1, length = 256)))
+dev.off()
