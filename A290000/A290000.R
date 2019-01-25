37bc588e32f311c1de291a8587feed9e0ce74012
Martin M. S. Pedersen <traxplayer@gmail.com>
2019-01-25 13:27:34 +0100

    small stuff

--- A290000/A290000.R
+++ A290000/A290000.R
@@ -3,15 +3,15 @@
 library(gmp)
 
 record <- 0
-p1 <- 2
+p1 <- 297501075797
 
 i <- 1
 
-setwd("OEIS/A290000")
+#setwd("OEIS/A290000")
 while(TRUE) {
   p2 <- nextprime(p1) 
   if (p2-p1 > record) {
-    sink("b290000", append = TRUE)
+    sink("b290000.txt", append = TRUE)
     cat(i,as.character(p1),"\n")
     i <- i + 1
     cat(i,as.character(p2),"\n")
